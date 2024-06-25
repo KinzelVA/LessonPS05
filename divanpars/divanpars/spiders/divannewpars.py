09e3b55e0f30abefddd13f4b484dbdc6f100d67b
KinzelVA <168530439+KinzelVA@users.noreply.github.com>
2024-06-25 12:15:30 +0700

    add divannewpars

--- divanpars/divanpars/spiders/divannewpars.py
+++ divanpars/divanpars/spiders/divannewpars.py
@@ -4,7 +4,15 @@ import scrapy
 class DivannewparsSpider(scrapy.Spider):
     name = "divannewpars"
     allowed_domains = ["https://divan.ru"]
-    start_urls = ["https://divan.ru/category/divany-i-kresla"]
+    start_urls = ["https://www.divan.ru/category/svet"]
 
     def parse(self, response):
-        pass
+        divans = response.css("div._c9h0M")
+        for divan in divans:
+            yield {
+                "name": divan.css("div.lsooF::text").get(),
+                "prise": divan.css("div.pY3d2 span::text").get(),
+                "url": divan.css("a").attrib["href"]
+            }
+
+
